fff14099a1568883915321abf6214ec260c9ae2d
emmaPlf <emma.palfi@edu.ece.fr>
2021-01-06 11:28:16 +0100

    Ajout denm

---
+++ catkin_ws/src/etsi_msgs/src/subscriber_denm.cpp
@@ -0,0 +1,15 @@
+#include "../../devel/include/etsi_msgs/DENM.h"
+#include "ros/ros.h"
+
+void messageCallback(const etsi_msgs::DENM::ConstPtr &msg) {
+  ROS_INFO("I have received a message denm : has situation : [%d]",
+           msg->has_situation);
+}
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "subscriber_denm");
+  ros::NodeHandle n;
+  ros::Subscriber sub = n.subscribe("topic_denm", 1000, messageCallback);
+  ros::spin();
+  return 0;
+}
