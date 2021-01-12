d61d9e0e25e8329b3cc1e28c89a84dbc6ecdc81e
tikystitch <36538825+tikystitch@users.noreply.github.com>
2021-01-12 12:08:13 +0100

    add sub and publi of CAM

--- catkin_ws/src/etsi_msgs/src/subscriber_cam.cpp
+++ catkin_ws/src/etsi_msgs/src/subscriber_cam.cpp
@@ -4,7 +4,18 @@
 // Permet de récupérer les infos du message CAM
 void messageCallback(const etsi_msgs::CAM::ConstPtr &msg) {
   ROS_INFO("I have received a message cam");
-  ROS_INFO("/n count: [%d]", msg->header.seq);
+  //   ROS_INFO("/n count: [%d]", msg->header.seq);
+  ROS_INFO("/n posX: [%ld]", msg->reference_position.longitude);
+  ROS_INFO("/n posY: [%ld]", msg->reference_position.latitude);
+  ROS_INFO("/n posZ: [%d]", msg->reference_position.altitude);
+  ROS_INFO("/n confZ: [%d]", msg->reference_position.altitude.confidence);
+  ROS_INFO("/n Acceleration: [%d]",
+           msg->high_frequency_container.longitudinal_acceleration.value);
+  ROS_INFO("/n accel Long Conf: [%d]",
+           msg->high_frequency_container.longitudinal_acceleration.confidence);
+  ROS_INFO("/n yaw rate: [%d]", msg->high_frequency_container.yaw_rate.value);
+  ROS_INFO("/n yaw rate conf: [%d]",
+           msg->high_frequency_container.yaw_rate.confidence);
 }
 
 int main(int argc, char **argv) {
