9ed762a8819904dc7a0e96d82534d15530cf9d0c
David Olivares <davidolivares68@gmail.com>
2021-01-18 11:28:05 +0100

    map + 3 turtlebots controlables

--- ecebot/src/test.py
+++ ecebot/src/test.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # script marche chez moi avec python 2.7
 import random, math
 import rospy
@@ -11,15 +13,27 @@ def sender():
     global v, omega, T
     rospy.init_node('talker', anonymous=True)
 
-    pub = rospy.Publisher('/cmd_vel/', Twist, queue_size=10)
+    pub0 = rospy.Publisher('tb3_0/cmd_vel/', Twist, queue_size=10)
+    pub1 = rospy.Publisher('tb3_1/cmd_vel/', Twist, queue_size=10)
+    pub2 = rospy.Publisher('tb3_2/cmd_vel/', Twist, queue_size=10)
 
     rate = rospy.Rate(1/T)
     try:
-        while True:
-            msg = Twist()
-            msg.linear.x = 2
-            msg.angular.z = 0
-            pub.publish(msg)
+        while not rospy.is_shutdown():
+            msg0 = Twist()
+            msg0.linear.x = 0.5
+            msg0.angular.z = 0
+            pub0.publish(msg0)
+
+            msg1 = Twist()
+            msg1.linear.x = 2
+            msg1.angular.z = 0
+            pub1.publish(msg1)
+
+            msg2 = Twist()
+            msg2.linear.x = 1
+            msg2.angular.z = 0
+            pub2.publish(msg2)
             # rospy.loginfo("sent omega = %s", omega)
             # rospy.loginfo("sent v = %s", v)
             rate.sleep()
